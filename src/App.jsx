9c73fd0aed69f7935793b991e2a20481c9c4a29e
Sergio Duran <538ser@gmail.com>
2024-03-19 15:52:40 -0600

    guitar list done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Guitar from "./components/Guitar";
+import { db } from "./data/db";
+
 function App() {
+  const [data, setData] = useState(db);
+
   return (
     <>
-      <h1>Guitar LA</h1>
+      <Header />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar 
+              guitar={guitar} 
+              key={guitar.id} 
+            />
+          ))}
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
     </>
   );
 }
